82e82d4f5a2af6384d63ec338ff1406e20097caf
rtvdiaspo <39888726+rtvdiaspo@users.noreply.github.com>
2025-10-03 20:39:31 -0400

    Create firebase-config.js

---
+++ firebase/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzoyo9K3BctyRcCIcAAUaI2M7smKoHPmw",
+  authDomain: "webrtc-video-app-dd7dd.firebaseapp.com",
+  projectId: "webrtc-video-app-dd7dd",
+  appId: "1:343221501205:web:c971275afe6d51495d3d1c"
+};
+
+firebase.initializeApp(firebaseConfig);
